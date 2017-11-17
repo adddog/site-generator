cddfdd3fde320f60758ea79db299825793ea8488
samelie <sam@stinkdigital.com>
2017-11-17 13:24:54 -0800

    assets

--- bin/copy_dist_to.sh
+++ bin/copy_dist_to.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-cp -r dist ../_sites/samelie.com/www
+rm -rf ../_sites/samelie.com/www/dist
+cp -r dist ../_sites/samelie.com/www/dist
+cp readme.md ../_sites/samelie.com/www
+cd ../_sites/samelie.com/www/
+git aa
+git yolo
+git psadd
+gsutil -m cp -r -a public-read dist/assets gs://samrad-samelie/www-assets
